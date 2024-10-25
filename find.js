ea966e340f213953ee88b456e141956a8b526be6
koyo221 <koyo.takaya@plaid.co.jp>
2024-10-25 23:33:45 +0900

    update

--- find.js
+++ find.js
@@ -19,7 +19,7 @@ async function main() {
       bulk.insert({
         name: `User${i}`,
         age: Math.floor(Math.random() * 100),
-        country: `Country${i}`,
+        country: Math.random() > 0.5 ? "Japan" : "USA",
         favorites: [`Favorite${i}`],
       });
     }
@@ -30,7 +30,12 @@ async function main() {
 
     // Find users where age is 30 without index
     let result = await collection.find({ age: 30 }).explain("executionStats");
-    fs.appendFileSync("findResult.txt", `Find users where age is 30 without index: ${JSON.stringify(result.executionStats)}\n`);
+    fs.appendFileSync(
+      "findResult.txt",
+      `Find users where age is 30 without index: ${JSON.stringify(
+        result.executionStats
+      )}\n`
+    );
 
     // Remove index before each test
     await collection.dropIndexes();
@@ -38,68 +43,159 @@ async function main() {
     // Find users where age is 30 with index
     await collection.createIndex({ age: 1 });
     result = await collection.find({ age: 30 }).explain("executionStats");
-    fs.appendFileSync("findResult.txt", `Find users where age is 30 with index: ${JSON.stringify(result.executionStats)}\n`);
-
-    // Remove index before each test
-    await collection.dropIndexes();
-
-    // FindOne user whose age is 30 without index
-    result = await collection.findOne({ age: 30 });
-    fs.appendFileSync("findResult.txt", `FindOne user whose age is 30 without index: ${JSON.stringify(result)}\n`);
-
-    // Remove index before each test
-    await collection.dropIndexes();
-
-    // FindOne user whose age is 30 with index
-    await collection.createIndex({ age: 1 });
-    result = await collection.findOne({ age: 30 });
-    fs.appendFileSync("findResult.txt", `FindOne user whose age is 30 with index: ${JSON.stringify(result)}\n`);
+    fs.appendFileSync(
+      "findResult.txt",
+      `Find users where age is 30 with index: ${JSON.stringify(
+        result.executionStats
+      )}\n`
+    );
 
     // Remove index before each test
     await collection.dropIndexes();
 
     // Find users where age = 30 AND country = 'Japan' without index
-    result = await collection.find({ age: 30, country: 'Japan' }).explain("executionStats");
-    fs.appendFileSync("findResult.txt", `Find users where age = 30 AND country = 'Japan' without index: ${JSON.stringify(result.executionStats)}\n`);
+    result = await collection
+      .find({ age: 30, country: "Japan" })
+      .explain("executionStats");
+    fs.appendFileSync(
+      "findResult.txt",
+      `Find users where age = 30 AND country = 'Japan' without index: ${JSON.stringify(
+        result.executionStats
+      )}\n`
+    );
 
     // Remove index before each test
     await collection.dropIndexes();
 
     // Find users where age = 30 AND country = 'Japan' with single index on age
     await collection.createIndex({ age: 1 });
-    result = await collection.find({ age: 30, country: 'Japan' }).explain("executionStats");
-    fs.appendFileSync("findResult.txt", `Find users where age = 30 AND country = 'Japan' with single index on age: ${JSON.stringify(result.executionStats)}\n`);
+    result = await collection
+      .find({ age: 30, country: "Japan" })
+      .explain("executionStats");
+    fs.appendFileSync(
+      "findResult.txt",
+      `Find users where age = 30 AND country = 'Japan' with single index (age): ${JSON.stringify(
+        result.executionStats
+      )}\n`
+    );
+
+    // Remove index before each test
+    await collection.dropIndexes();
+
+    // Find users where age = 30 AND country = 'Japan' with compound index (age, country)
+    await collection.createIndex({ country: 1 });
+    result = await collection
+      .find({ age: 30, country: "Japan" })
+      .explain("executionStats");
+    fs.appendFileSync(
+      "findResult.txt",
+      `Find users where age = 30 AND country = 'Japan' with single index (country): ${JSON.stringify(
+        result.executionStats
+      )}\n`
+    );
 
     // Remove index before each test
     await collection.dropIndexes();
 
     // Find users where age = 30 AND country = 'Japan' with compound index (age, country)
     await collection.createIndex({ age: 1, country: 1 });
-    result = await collection.find({ age: 30, country: 'Japan' }).explain("executionStats");
-    fs.appendFileSync("findResult.txt", `Find users where age = 30 AND country = 'Japan' with compound index (age, country): ${JSON.stringify(result.executionStats)}\n`);
+    result = await collection
+      .find({ age: 30, country: "Japan" })
+      .explain("executionStats");
+    fs.appendFileSync(
+      "findResult.txt",
+      `Find users where age = 30 AND country = 'Japan' with compound index (age, country): ${JSON.stringify(
+        result.executionStats
+      )}\n`
+    );
 
     // Remove index before each test
     await collection.dropIndexes();
 
     // Find users where 20 <= age <= 30 without index
-    result = await collection.find({ age: { $gte: 20, $lte: 30 } }).explain("executionStats");
-    fs.appendFileSync("findResult.txt", `Find users where 20 <= age <= 30 without index: ${JSON.stringify(result.executionStats)}\n`);
+    result = await collection
+      .find({ age: { $gte: 20, $lte: 30 } })
+      .explain("executionStats");
+    fs.appendFileSync(
+      "findResult.txt",
+      `Find users where 20 <= age <= 30 without index: ${JSON.stringify(
+        result.executionStats
+      )}\n`
+    );
 
     // Remove index before each test
     await collection.dropIndexes();
 
     // Find users where 20 <= age <= 30 with index
     await collection.createIndex({ age: 1 });
-    result = await collection.find({ age: { $gte: 20, $lte: 30 } }).explain("executionStats");
-    fs.appendFileSync("findResult.txt", `Find users where 20 <= age <= 30 with index: ${JSON.stringify(result.executionStats)}\n`);
+    result = await collection
+      .find({ age: { $gte: 20, $lte: 30 } })
+      .explain("executionStats");
+    fs.appendFileSync(
+      "findResult.txt",
+      `Find users where 20 <= age <= 30 with index on age: ${JSON.stringify(
+        result.executionStats
+      )}\n`
+    );
 
     // Remove index before each test
     await collection.dropIndexes();
 
     // Find users where 20 <= age <= 30 ORDER BY country
-    result = await collection.find({ age: { $gte: 20, $lte: 30 } }).sort({ country: 1 }).explain("executionStats");
-    fs.appendFileSync("findResult.txt", `Find users where 20 <= age <= 30 ORDER BY country: ${JSON.stringify(result.executionStats)}\n`);
+    result = await collection
+      .find({ age: { $gte: 20, $lte: 30 } })
+      .sort({ country: 1 })
+      .explain("executionStats");
+    fs.appendFileSync(
+      "findResult.txt",
+      `Find users where 20 <= age <= 30 ORDER BY country: ${JSON.stringify(
+        result.executionStats
+      )}\n`
+    );
 
+    await collection.dropIndexes();
+
+    // Find users where 20 <= age <= 30 ORDER BY country
+    await collection.createIndex({ country: 1 });
+    result = await collection
+      .find({ age: { $gte: 20, $lte: 30 } })
+      .sort({ country: 1 })
+      .explain("executionStats");
+    fs.appendFileSync(
+      "findResult.txt",
+      `Find users where 20 <= age <= 30 ORDER BY country with index (country): ${JSON.stringify(
+        result.executionStats
+      )}\n`
+    );
+    await collection.dropIndexes();
+
+    // Find users where 20 <= age <= 30 ORDER BY country
+    await collection.createIndex({ age: 1 });
+    result = await collection
+      .find({ age: { $gte: 20, $lte: 30 } })
+      .sort({ country: 1 })
+      .explain("executionStats");
+    fs.appendFileSync(
+      "findResult.txt",
+      `Find users where 20 <= age <= 30 ORDER BY country with index (age): ${JSON.stringify(
+        result.executionStats
+      )}\n`
+    );
+    await collection.dropIndexes();
+
+    // Find users where 20 <= age <= 30 ORDER BY country
+    await collection.createIndex({ age: 1, country: 1 });
+    result = await collection
+      .find({ age: { $gte: 20, $lte: 30 } })
+      .sort({ country: 1 })
+      .explain("executionStats");
+    fs.appendFileSync(
+      "findResult.txt",
+      `Find users where 20 <= age <= 30 ORDER BY country with index (age, country): ${JSON.stringify(
+        result.executionStats
+      )}\n`
+    );
+    await collection.dropIndexes();
   } finally {
     await client.close();
   }
--- insert.js
+++ insert.js
@@ -74,9 +74,77 @@ async function main() {
     fs.appendFileSync("insertResult.txt", result);
 
     await collection.deleteMany({});
+    await collection.dropIndexes();
+
+    // insert complex documents
+    const randomObj = generateRandomObject(100);
+    const arrRandom = Array.from({ length: 100000 }, () => ({ ...randomObj }));
+    startTime = Date.now();
+
+    for (let i = 0; i < INSERTION_COUNT; i++) {
+      await collection.insertOne(arrRandom[i]);
+    }
+    endTime = Date.now();
+    duration = endTime - startTime;
+    result = `100,000 insertion of complex documents: ${duration}ms\n`;
+    fs.appendFileSync("insertResult.txt", result);
+
+    await collection.deleteMany({});
+
+    // insert nested documents
+    const nestedObj = createNestedObject(100);
+    const arrNested = Array.from({ length: 100000 }, () => ({ ...nestedObj }));
+    startTime = Date.now();
+
+    for (let i = 0; i < INSERTION_COUNT; i++) {
+      await collection.insertOne(arrNested[i]);
+    }
+    endTime = Date.now();
+    duration = endTime - startTime;
+    result = `100,000 insertion of nested documents: ${duration}ms\n`;
+    fs.appendFileSync("insertResult.txt", result);
+
+    await collection.deleteMany({});
   } finally {
     await client.close();
   }
+}
+
+function generateRandomObject(
+  count,
+  keyLength = 8,
+  chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+) {
+  const result = {};
+  const usedKeys = new Set();
+
+  function generateRandomKey() {
+    let key = "";
+    for (let i = 0; i < keyLength; i++) {
+      const randomIndex = Math.floor(Math.random() * chars.length);
+      key += chars[randomIndex];
+    }
+    return key;
+  }
+
+  while (Object.keys(result).length < count) {
+    const key = generateRandomKey();
+    if (!usedKeys.has(key)) {
+      usedKeys.add(key);
+      result[key] = Object.keys(result).length + 1;
+    }
+  }
+
+  return result;
+}
+
+function createNestedObject(depth) {
+  if (depth <= 0) {
+    return { end: true };
+  }
+  return {
+    child: createNestedObject(depth - 1),
+  };
 }
 
 main().catch(console.error);
